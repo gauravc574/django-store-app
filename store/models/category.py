12aa13778b6b32780817335be82892c1e90bda3f
gauravc574 <gauravchawre580@gmail.com>
2022-08-01 23:13:11 +0530

    model classes addded

---
+++ store/models/category.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self) -> str:
+        return self.name
--- store/models/product.py
+++ store/models/product.py
@@ -1,8 +1,18 @@
 from distutils.command.upload import upload
+from unicodedata import category
 from django.db import models
+from .category import Category
 
 class Product(models.Model):
     name =  models.CharField(max_length=50)
     price = models.IntegerField(default=0)
-    description = models.CharField(max_length=200, default="")
-    image = models.ImageField(upload_to = "products/")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1) #on deletion of category respective products will be deleted
+    description = models.CharField(max_length=200, default="", null=True, blank=True)
+    image = models.ImageField(upload_to = "uploads/products/")
+
+    # def __str__(self) -> str:
+    #     return self.name
+
+    @staticmethod
+    def get_all_products():
+        return Product.objects.all()
